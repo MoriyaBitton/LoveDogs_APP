6683bcdae8121b385da0c6ea52ab6d31d94d5555
Mike <Miket9717@gmail.com>
2021-11-30 23:37:58 +0200

    added demo of text and button

--- App/src/main/java/com/basic/lvd/MainActivity.java
+++ App/src/main/java/com/basic/lvd/MainActivity.java
@@ -3,6 +3,8 @@ package com.basic.lvd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //R.id.testKek
+    }
+
+    public void OnClick(View button){
+        TextView text = findViewById(R.id.testKek);
+        TextView name = findViewById(R.id.userName);
+        text.setText("Hello, " + name.getText());
+        button.setEnabled(false);
     }
 }
