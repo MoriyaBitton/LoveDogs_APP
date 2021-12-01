cc36ef7c0429f4962cf3da4a646110d60e1f434e
Mike <Miket9717@gmail.com>
2021-12-01 23:48:13 +0200

    two screens, bad way

---
+++ App/src/main/java/com/basic/lvd/CreateEvent.java
@@ -0,0 +1,15 @@
+package com.basic.lvd;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateEvent extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hello_user);
+
+        //R.id.testKek
+    }
+}
--- App/src/main/java/com/basic/lvd/MainActivity.java
+++ App/src/main/java/com/basic/lvd/MainActivity.java
@@ -2,12 +2,15 @@ package com.basic.lvd;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    boolean connected = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,9 +19,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void OnClick(View button){
-        TextView text = findViewById(R.id.testKek);
-        TextView name = findViewById(R.id.userName);
-        text.setText("Hello, " + name.getText());
-        button.setEnabled(false);
+        if(!connected) {
+            TextView text = findViewById(R.id.testKek);
+            TextView name = findViewById(R.id.userName);
+            text.setText("Hello, " + name.getText());
+            //button.setEnabled(false);
+            connected = true;
+            Log.d("mylog", "hello," + name.getText());
+        }else if(connected) {
+            //Intent intent = new Intent(this, CreateEvent.class);
+            TextView name = findViewById(R.id.userName);
+            CharSequence username = name.getText();
+            setContentView(R.layout.hello_user);
+            TextView text = findViewById(R.id.welcomeText);
+            text.setText("Hello, " + name.getText());
+        }
     }
 }
