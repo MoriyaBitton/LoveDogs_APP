6f98fd91059a7d241ec81f72a30c7c9125bbd532
Itay Karavani <74235390+itaygrx@users.noreply.github.com>
2021-12-16 12:31:23 +0200

    settings changed

--- App/src/main/java/com/example/love_dogs/ui/settings/SettingsFragment2.java
+++ App/src/main/java/com/example/love_dogs/ui/settings/SettingsFragment2.java
@@ -1,7 +1,15 @@
 package com.example.love_dogs.ui.settings;
 
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.preference.PreferenceFragmentCompat;
 
 import com.example.love_dogs.R;
@@ -12,4 +20,24 @@ public class SettingsFragment2 extends PreferenceFragmentCompat {
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey);
     }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = super.onCreateView(inflater, container, savedInstanceState);
+        view.setBackground(getResources().getDrawable(R.drawable.big_round));
+        return view;
+    }
+
+//    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+//        TypedValue typedValue = new TypedValue();
+//        Resources.Theme theme = requireContext().getTheme();
+//
+//        theme.resolveAttribute(R.color.teal_200,typedValue,true);
+//        int color = typedValue.data;
+//
+//        view.setBackgroundColor(color);
+//
+//        super.onViewCreated(view, savedInstanceState);
+//    }
 }
