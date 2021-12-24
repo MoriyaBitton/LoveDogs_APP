1fe1d89b597e8d0ff1e1b838c8c11867dc249ac7
Itay Karavani <itaygrx@gmail.com>
2021-12-24 15:24:27 +0200

    little changes

--- App/src/main/java/com/example/love_dogs/PicHandler.java
+++ App/src/main/java/com/example/love_dogs/PicHandler.java
@@ -60,15 +60,18 @@ public class PicHandler extends AppCompatActivity {
 
     private void checkFilePermissions(){
 
+        int requestCode = 0;
         if(Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP){
+
                 int permissionCheck = PicHandler.this.checkSelfPermission("Manifest.permission.READ_EXTERNAL_STORAGE");
                 permissionCheck += PicHandler.this.checkSelfPermission("Manifest.permission.WRITE_EXTERNAL_STORAGE");
                 if(permissionCheck != 0){
                     this.requestPermissions(new String[]{Manifest.permission.
-                            READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE},);
+                            READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE},requestCode);
                 }
         }
         }
+
     /* Function to upload images to Firebase storage */
     public void uploadToStorage(){
 
